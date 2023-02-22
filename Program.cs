3eacd0f5b62915d571310d8e6a7c00e1f6f54421
Meghrig Terzian <76222303+meghrigJAC@users.noreply.github.com>
2023-02-22 11:14:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MIN_DAYS = 0, MAX_DAYS = 31;
+
+            int numberOfDays = GetNumber("Please enter the number of days you exercised this month ", MIN_DAYS, MAX_DAYS);
+
+            
+        }
+
+        public static int GetNumber(string msg, int min, int max)
+        {
+            int number;
+
+            Console.Write(msg);
+            bool valid = int.TryParse(Console.ReadLine(), out number);
+
+            while (!valid || number < min || number > max)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("$Error invalid number: {msg}");
+                Console.ForegroundColor = ConsoleColor.White;
+                valid = int.TryParse(Console.ReadLine(), out number);
+
+            }
+
+            return number;
+        }
+
+    }
+}
